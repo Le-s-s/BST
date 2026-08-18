d13d0ad3c27adb28c4cd2d1e9d93268bc3abf19c
Le-s-s <lexton.d.lewis1@gmail.com>
2026-08-18 16:44:34 -0600

    Added includes function, and display for node tree

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -12,34 +12,53 @@ class tree {
   }
   buildTree(arr) {
     const sortedArr = [...new Set(arr)].sort((a, b) => a - b);
+    if (sortedArr.length === 0) {
+      return null;
+    }
+    const middleIndex = Math.floor(sortedArr.length / 2);
     const middle = new node();
-    middle.value = sortedArr[Math.floor(sortedArr.length / 2)];
-    sortedArr.forEach((num) => {
-        let current = middle
-      if (current.leftChild === null) {
-        if (num < current.value) {
-          let temp = new node();
-          temp.value = num;
-          current.leftChild = temp;
-          current = current.leftChild
-        }
-      }
-      if (current.rightChild === null) {
-        if (num > current.value) {
-          let temp = new node();
-          temp.value = num;
-          current.rightChild = temp;
-          current = current.rightChildChild
-        }
-      }
-    });
+    middle.value = sortedArr[middleIndex];
+
+    middle.leftChild = this.buildTree(sortedArr.slice(0, middleIndex));
+    middle.rightChild = this.buildTree(sortedArr.slice(middleIndex + 1));
+
     console.log(sortedArr);
     console.log(middle);
     return middle;
   }
+  includes(value) {
+    // traverse each side of the root node until value is found.
+    let current = this.root;
+    while (current !== null) {
+      if (current.value === value) {
+        return true;
+      }
+      if (value < current.value) {
+        current = current.leftChild;
+      } else {
+        current = current.rightChild;
+      }
+    }
+    return false;
+  }
 }
 
 const thang = new tree([9, 4, 9, 1, 2, 6, 8, 2, 4, 6]);
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null || node === undefined) {
+    return;
+  }
+
+  prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+  prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
+};
+prettyPrint(thang);
+console.log(thang.includes(0));
+console.log(thang.includes(7));
+console.log(thang.includes(1));
+console.log(thang.includes(11));
 
 //first, get the arr during creationg of the tree
 // instantly run through buildtree
