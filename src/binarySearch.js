42ea4708ff53a66a8102c7d02f63d92895e7307e
Le-s-s <lexton.d.lewis1@gmail.com>
2026-08-27 15:57:15 -0600

    Began adding delete function to bst

--- src/binarySearch.js
+++ src/binarySearch.js
@@ -22,8 +22,6 @@ class tree {
     middle.leftChild = this.buildTree(sortedArr.slice(0, middleIndex));
     middle.rightChild = this.buildTree(sortedArr.slice(middleIndex + 1));
 
-    console.log(sortedArr);
-    console.log(middle);
     return middle;
   }
   includes(value) {
@@ -41,24 +39,93 @@ class tree {
     }
     return false;
   }
+  insert(num) {
+    // go down each side of node add node value to array
+    // add num to array
+    //sort array
+    //call build again
+  }
+
+  deleteItem(value) {
+    let current = this.root;
+    let previous;
+    while (true) {
+      if (current === null) return;
+      if (current.value === value) {
+        if (typeof previous === "undefined") {
+          return "Not yet."
+        }
+        if (current.rightChild === null) {
+          if (current.value > previous.value) {
+            previous.rightChild = current.leftChild;
+          }
+          if (current.value < previous.value) {
+            previous.leftChild = current.leftChild;
+          }
+        }
+        if (current.leftChild === null) {
+          if (current.value > previous.value) {
+            previous.rightChild = current.rightChild;
+          }
+          if (current.value < previous.value) {
+            previous.leftChild = current.rightChild;
+          }
+        }
+        if (current.rightChild !== null && current.leftChild !== null) {
+          if (current.value > previous.value) {
+            current.rightChild.leftChild = current.leftChild;
+            previous.rightChild = current.rightChild;
+          }
+          if (current.value < previous.value) {
+            current.rightChild.leftChild = current.leftChild;
+            previous.leftChild = current.rightChild;
+          }
+        }
+
+        return;
+      }
+      if (value < current.value) {
+        previous = current;
+        current = current.leftChild;
+      } else {
+        previous = current;
+        current = current.rightChild;
+      }
+    }
+    return false;
+  }
+  prettyPrint = (node = this.root, prefix = "", isLeft = true) => {
+    let current = node;
+    if (current === null || current === undefined) {
+      return;
+    }
+
+    this.prettyPrint(
+      current.rightChild,
+      `${prefix}${isLeft ? "│   " : "    "}`,
+      false,
+    );
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${current.value}`);
+    this.prettyPrint(
+      current.leftChild,
+      `${prefix}${isLeft ? "    " : "│   "}`,
+      true,
+    );
+  };
 }
 
 const thang = new tree([9, 4, 9, 1, 2, 6, 8, 2, 4, 6]);
 
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-  if (node === null || node === undefined) {
-    return;
-  }
-
-  prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
-  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
-  prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
-};
-prettyPrint(thang);
+thang.prettyPrint();
+console.log(thang.includes(9));
+console.log(thang.deleteItem(9));
+console.log(thang.deleteItem(2));
 console.log(thang.includes(0));
 console.log(thang.includes(7));
 console.log(thang.includes(1));
 console.log(thang.includes(11));
+console.log(thang.includes(9));
+thang.prettyPrint();
 
 //first, get the arr during creationg of the tree
 // instantly run through buildtree
